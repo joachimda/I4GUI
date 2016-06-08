e6b9538b25c53cf3b69e22395c9d713604557c0a
joachimda <joachimdamandersen@gmail.com>
2016-06-08 15:35:07 +0200

    done with sailboat class

---
+++ Lab 21 - OOP and JavaScript/HullSpeedWeb/SailboatClass.js
@@ -0,0 +1,24 @@
+﻿function sailboat() {
+    var name;
+    var lenght;
+
+    //Get and set functions
+    this.setName = function(value) {
+        name = value;
+    }
+    this.setLenght = function(value) {
+        lenght = value;
+    }
+    this.getName = function() {
+        return name;
+    }
+    this.getLenght = function() {
+        return lenght;
+    }
+
+    //Member function to calculate the hullspeed
+    this.hullSpeed = function() {
+        return 1.34 * Math.sqrt(lenght);
+    }
+}
+
