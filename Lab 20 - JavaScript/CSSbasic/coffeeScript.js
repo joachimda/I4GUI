0de01214a4ee2e902d2485f20ac2b4457e33f21c
joachimda <joachimdamandersen@gmail.com>
2016-06-08 15:02:47 +0200

    .js included

---
+++ Lab 20 - JavaScript/CSSbasic/coffeeScript.js
@@ -0,0 +1,11 @@
+﻿var menu = document.getElementsByClassName("menu");
+for (i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("mouseover", function (event) {
+        var target = event.target || event.srcElement;
+        target.className = "menuEnhanced"
+    });
+    menu[i].addEventListener("mouseout", function (event) {
+        var target = event.target || event.srcElement;
+        target.className = "menu";
+    });
+};
