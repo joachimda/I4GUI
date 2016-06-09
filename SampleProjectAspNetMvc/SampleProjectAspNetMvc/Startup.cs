6568022c3111296622396e82555d4024ed3044a2
joachimda <joachimdamandersen@gmail.com>
2016-06-09 11:56:34 +0200

    added index for jobs page

---
+++ SampleProjectAspNetMvc/SampleProjectAspNetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleProjectAspNetMvc.Startup))]
+namespace SampleProjectAspNetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
