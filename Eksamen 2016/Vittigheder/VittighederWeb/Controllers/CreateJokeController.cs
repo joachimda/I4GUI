345660bf03a70d3c6683f42265321b902f8ef752
joachimda <joachimdamandersen@gmail.com>
2016-06-13 21:33:20 +0200

    created Joke class in models

---
+++ Eksamen 2016/Vittigheder/VittighederWeb/Controllers/CreateJokeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VittighederWeb.Controllers
+{
+    public class CreateJokeController : Controller
+    {
+        // GET: CreateJoke
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Eksamen 2016/Vittigheder/VittighederWeb/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VittighederWeb.Models
+{
+    public class Joke
+    {
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Author { get; set; }  
+    }
+}
