b09acc3594534d844308b2f17150d6f211e48276
joachimda <joachimdamandersen@gmail.com>
2016-06-13 11:18:33 +0200

    created mainWindow

--- Eksamen 2016/Vittigheder/VittighederWpf/MainWindow.xaml.cs
+++ Eksamen 2016/Vittigheder/VittighederWpf/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace VittighederWpf
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
