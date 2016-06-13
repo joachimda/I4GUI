919e810d6b3f265e279d5557ad0044a59d16a9c6
joachimda <joachimdamandersen@gmail.com>
2016-06-13 21:37:38 +0200

    created Serialize method

---
+++ Eksamen 2016/Vittigheder/VittighederWeb/Tools/JsonToolbox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using VittighederWeb.Models;
+
+namespace VittighederWeb.Tools
+{
+    public class JsonToolbox
+    {
+        private readonly string _path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\webData.json";
+
+
+        /// <summary>
+        /// Serializes a list of Joke objects and writes them to a file
+        /// </summary>
+        /// <param name="list">The list of jokes to be serialized</param>
+        public void Serialize(List<Joke> list)
+        {
+            string jsonOut = JsonConvert.SerializeObject(list);
+            using (StreamWriter streamWriter = new StreamWriter(_path))
+            {
+                streamWriter.Write(jsonOut);
+                streamWriter.Close();
+            }
+        }
+    }
+}
