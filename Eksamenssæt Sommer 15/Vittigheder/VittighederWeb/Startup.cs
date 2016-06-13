9b6c57f890e90bcffaf51a238d20ad3ff128e2e2
joachimda <joachimdamandersen@gmail.com>
2016-06-13 09:25:22 +0200

    Added new ASP.Net MVC project

---
+++ Eksamenssæt Sommer 15/Vittigheder/VittighederWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VittighederWeb.Startup))]
+namespace VittighederWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
