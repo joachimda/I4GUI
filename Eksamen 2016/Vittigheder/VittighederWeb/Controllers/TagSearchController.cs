b17ff244718f7bab8232a9ec2b1d2d97483a592a
joachimda <joachimdamandersen@gmail.com>
2016-06-14 02:27:02 +0200

    wat?

---
+++ Eksamen 2016/Vittigheder/VittighederWeb/Controllers/TagSearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VittighederWeb.Controllers
+{
+    public class TagSearchController : Controller
+    {
+        // GET: TagSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Search(string query)
+        {
+            ViewData["id"] = query;
+            return View("Result");
+        }
+    }
+}
